0ef809b78391341259010b6bb6bf01c812f44001
pushpendra.khandelwal@kuliza.com <pushpendra.khandelwal@kuliza.com>
2018-05-20 12:15:27 +0530

    Discrete slider added

--- SliderDemo/DiscreteSlider.swift
+++ SliderDemo/DiscreteSlider.swift
@@ -1,27 +1,16 @@
 //
-//  ViewController.swift
+//  DiscreteSlider.swift
 //  SliderDemo
 //
-//  Created by Pushpendra Khandelwal on 09/05/18.
+//  Created by Pushpendra Khandelwal on 20/05/18.
 //  Copyright © 2018 Pushpendra Khandelwal. All rights reserved.
 //
 
 import UIKit
 
-import UIKit
-
-class ViewController: UIViewController {
-    
-    @IBOutlet weak var customSlider: DiscreteSlider!
+class DiscreteSlider: UISlider {
     
     private let step: Float = 10
-    @IBOutlet weak var slider: UISlider! {
-        didSet {
-            slider.addTarget(self, action: #selector(didValueChange), for: .valueChanged)
-        }
-    }
-    
-    @IBOutlet weak var outputLabel: UILabel!
     
     // MARK: -PROPERTIES OF BOTTOM LABEL W.R.T. Slider
     var stepY: CGFloat = 30
@@ -44,7 +33,7 @@ class ViewController: UIViewController {
         return umbrellaLabel
     }()
     
-    var indicatorY: CGFloat = 30
+    var indicatorHeight: CGFloat = 30
     var sliderAnimationTime: TimeInterval = 0.10
     
     // MARK: - PROPERTIES OF SLIDER
@@ -58,61 +47,70 @@ class ViewController: UIViewController {
         return model
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        let height = stepY + stepHeight + descriptionY + descriptionHeight + thumbWidth
+        return CGSize(width: bounds.width, height: height)
+    }
+    
+    func setup() {
+        addTarget(self, action: #selector(didValueChange), for: .valueChanged)
         setupSlider()
         addIndicatorView()
-        setDefaultValue()
-        customSliderThings()
+        
+    }
+    
+    private func setDefaultValue() {
+        value = sliderModel.defualtValue
+        updateTopView()
     }
     
     private func setupSlider() {
-        slider.minimumValue = sliderModel.minimum
-        slider.maximumValue = sliderModel.maximum
+        minimumValue = sliderModel.minimum
+        maximumValue = sliderModel.maximum
         createDiscretePoints(with: sliderModel.discretePoint)
     }
     
     func createDiscretePoints(with discretePoint: [Float]) {
-        view.layoutIfNeeded()
+        layoutIfNeeded()
         discretePoint.forEach { [unowned self] in
             self.setLabelForValue(Float($0))
         }
+        setDefaultValue()
     }
     
     private func addIndicatorView() {
-        view.addSubview(indicator)
-    }
-    
-    private func setDefaultValue() {
-        slider.value = sliderModel.defualtValue
-        updateTopView()
+        addSubview(indicator)
     }
     
     private func setLabelForValue(_ value: Float) {
-        self.slider.value = value
+        self.value = value
         createStepLabel()
     }
     
     private func createStepLabel() {
         let stepX = CGFloat(xPositionFromSliderValue())
-        let yPos = stepY + self.slider.frame.origin.y
+        let yPosition: CGFloat = stepY + thumbWidth
         
         let frame = CGRect.init(x: stepX,
-                                y: yPos,
+                                y: yPosition,
                                 width: stepWidth,
                                 height: stepHeight)
         
         let stepLabel = UILabel.init(frame: frame)
         stepLabel.font = stepFont
         stepLabel.text = "|"
-        self.view.addSubview(stepLabel)
+        addSubview(stepLabel)
         
         let desFrame = CGRect.init(x: stepX - descriptionX,
-                                   y: yPos + descriptionY,
+                                   y: yPosition + descriptionY,
                                    width: descriptionWidth,
                                    height: descriptionHeight)
         createDesciptionLabel(desFrame)
@@ -124,44 +122,42 @@ class ViewController: UIViewController {
         descriptionLabel.textAlignment = .center
         descriptionLabel.font = descriptionFont
         descriptionLabel.text = sliderValueInString()
-        self.view.addSubview(descriptionLabel)
+        addSubview(descriptionLabel)
     }
     
     func updateTopView() {
         let x = CGFloat(xPositionFromSliderValue()) - (thumbWidth / 2.0)
-        let y = slider.frame.origin.y - indicatorY
+        let y = bounds.origin.y
         let indicatorFrame =  CGRect.init(x: x, y: y, width: thumbWidth, height: thumbWidth)
         indicator.frame = indicatorFrame
     }
     
     func xPositionFromSliderValue() -> Float {
-        let sliderRange = slider.frame.size.width - thumbWidth;
-        let sliderOrigin = slider.frame.origin.x + (thumbWidth / 2.0)
+        let sliderRange = frame.size.width - thumbWidth;
+        let sliderOrigin = (thumbWidth / 2.0)
         
-        let xPosFromSliderStart = (((slider.value - slider.minimumValue) / (slider.maximumValue - slider.minimumValue)) * Float(sliderRange)) + Float(sliderOrigin)
+        let xPosFromSliderStart = (((value - minimumValue) / (maximumValue - minimumValue)) * Float(sliderRange)) + Float(sliderOrigin)
         return xPosFromSliderStart
     }
     
     // FIXME: - CHANGE IT
     private func sliderValueInString() -> String {
-        return slider.value.description
+        return value.description
     }
     
     @objc func didValueChange() {
         
-        let roundedVal = round(slider.value / self.step) * self.step
-    
-        UIView.animate(withDuration: sliderAnimationTime, animations: {
-            self.slider.setValue(roundedVal, animated: true)
+        let roundedVal = round(value / self.step) * self.step
+        
+        UIView.animate(withDuration: sliderAnimationTime, animations: { [unowned self] in
+            self.setValue(roundedVal, animated: true)
         }) { [unowned self] _ in
             self.updateTopView()
-            self.outputLabel.text = roundedVal.description
         }
     }
     
-    func customSliderThings() {
-        customSlider.setup()
+    override func trackRect(forBounds bounds: CGRect) -> CGRect {
+        let trackRect = super.trackRect(forBounds: bounds)
+        return CGRect(x: trackRect.origin.x, y: indicatorHeight + thumbWidth/2, width: trackRect.width, height: trackRect.height)
     }
-
 }
-
